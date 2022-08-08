1473d8c088a91b609af014d41bad686d33b87fd7
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-08 16:53:31 +0200

    Create whatbook.ts
    Second class with the export created.

---
+++ src/whatbook.ts
@@ -0,0 +1,14 @@
+export class WhatBook {
+
+    private _authorName: string;
+    private _bookTitle: string;
+
+    constructor(an: string, bt:string) {
+        this._authorName = an;
+        this._bookTitle = bt;
+    }
+
+
+
+
+}
