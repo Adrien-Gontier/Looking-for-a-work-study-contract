ce8be572cbd38da67a5067180d6487b0fb6361c2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-08 16:54:47 +0200

    Create whatbook.js
    JS files of whatbook.ts created.

---
+++ dist/whatbook.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WhatBook = void 0;
+class WhatBook {
+    constructor(an, bt) {
+        this._authorName = an;
+        this._bookTitle = bt;
+    }
+}
+exports.WhatBook = WhatBook;
