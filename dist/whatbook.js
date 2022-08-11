5cbd137a964596018f87927d2aab1c3bac3acfe8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-11 16:02:39 +0200

    Update whatbook.js
    Line 9-20 : JavaScript file tanspiled from TypeScript file.

--- dist/whatbook.js
+++ dist/whatbook.js
@@ -6,5 +6,17 @@ class WhatBook {
         this._authorName = an;
         this._bookTitle = bt;
     }
+    get authorName() {
+        return this._authorName;
+    }
+    set authorName(authorName) {
+        this._authorName = authorName;
+    }
+    get bookTitle() {
+        return this._bookTitle;
+    }
+    set bookTitle(bookTitle) {
+        this._bookTitle = bookTitle;
+    }
 }
 exports.WhatBook = WhatBook;
