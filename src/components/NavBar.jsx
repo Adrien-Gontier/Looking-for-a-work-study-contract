876f4318badd421bd5bcf16432340e9e3dda4dde
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-06 15:33:38 +0200

    Update NavBar.jsx
    Links to mini applications added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div>
+            <Link>Couting</Link>
+            <Link>Timer</Link>
+            <Link>Simple calculator</Link>
+            <Link>m,n,k-game</Link>
+            <Link>Just Price</Link>
+            <Link>Clock / Date</Link>
+            <Link>Eight Ball</Link>
+        </div>
+    )
 }
