ec6749a795a8f7b5f3f4b822baf1b82a0db5bbd7
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-05-11 20:36:31 +0200

    Update DateAndTime.jsx
    Comments added

--- src/pages/DateAndTime.jsx
+++ src/pages/DateAndTime.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 export default function DateAndTime() {
+
+  // Date.nom
+
+  // date month
+
+  // date year
+
+  // hour now
+
+  // Think to adjust time and date
+
   return (
     <div>DateAndTime</div>
   )
