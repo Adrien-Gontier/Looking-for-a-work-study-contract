f11dcf909a9515459b36b03b4fde6d220ba65860
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-10-28 10:20:13 +0200

    Update Counting.jsx
    Title of mini app added to the mini app page

--- src/pages/Counting.jsx
+++ src/pages/Counting.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import TitleApp from '../components/counting/TitleApp';
 
 export default function Counting() {
+
+    const titleMiniApp = TitleApp;
+
+
     return <div>Counting</div>
 }
