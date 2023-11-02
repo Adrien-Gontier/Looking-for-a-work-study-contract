aba75ac31fdbb543365fa30a3fe9deb50d6ccbe0
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-02 19:37:28 +0100

    Update SimpleCalculator.jsx
    Title of mini app added to the mini app page.

--- src/pages/SimpleCalculator.jsx
+++ src/pages/SimpleCalculator.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import TitleApp from '../components/simple_calculator/TitleApp'
 
 export default function SimpleCalculator() {
+
+    const titleMiniApp = TitleApp;
+
+
     return <div>SimpleCalculator</div>
 }
