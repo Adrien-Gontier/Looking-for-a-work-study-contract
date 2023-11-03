030361c82e497c9ccf65ce0e7c205d984d5f5482
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-03 21:26:33 +0100

    Update Timer.jsx
    Title of mini app added to the mini app page.

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import TitleApp from '../components/timer/TitleApp'
 
 export default function Timer() {
+
+    const titleMiniApp = TitleApp;
+
+
+
+
     return <div>Timer</div>
 }
