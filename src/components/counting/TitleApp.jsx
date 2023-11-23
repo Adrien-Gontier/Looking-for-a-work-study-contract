e8c8631fc583037c4d3c4926ef873c0e7b512f97
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-23 18:03:12 +0100

    Update TitleApp.jsx
    Code format improved

--- src/components/counting/TitleApp.jsx
+++ src/components/counting/TitleApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function TitleApp() {
-  return (
-    <div className='counting-App-Title'>Counting</div>
-  )
+    return <div className="counting-App-Title">Counting</div>
 }
