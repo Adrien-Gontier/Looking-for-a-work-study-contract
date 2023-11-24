88ec1c88ac35a08d0d06d9307417f458b02b8a2c
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-24 21:33:50 +0100

    Update TitleApp.jsx
    Code format improved

--- src/components/date_and_time/TitleApp.jsx
+++ src/components/date_and_time/TitleApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function TitleApp() {
-  return (
-    <div className='dateAndTime-app-title'>Date and Time</div>
-  )
+    return <div className="dateAndTime-app-title">Date and Time</div>
 }
