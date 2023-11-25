218643a37620686aa4590d17d38415f507b6dd4a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-25 08:01:41 +0100

    Update TitleApp.jsx
    Code format improved

--- src/components/home/TitleApp.jsx
+++ src/components/home/TitleApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function TitleApp() {
-  return (
-    <div className='home-app-title'>Home</div>
-  )
+    return <div className="home-app-title">Home</div>
 }
