9282874730c0c3130b4046affcaf94c0f13f444f
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-27 22:06:14 +0100

    Update TitleApp.jsx
    Code format improved

--- src/components/mnkgame/TitleApp.jsx
+++ src/components/mnkgame/TitleApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function TitleApp() {
-  return (
-    <div className='MNKGame-app-title'>M N K Game</div>
-  )
+    return <div className="MNKGame-app-title">M N K Game</div>
 }
