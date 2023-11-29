97648304e05e6cde3f82d0e66e36dd9da8ec6316
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-11-29 18:09:00 +0100

    Update TitleApp.jsx
    Code format improved

--- src/components/timer/TitleApp.jsx
+++ src/components/timer/TitleApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function TitleApp() {
-  return (
-    <div className='timer-app-title'>Timer</div>
-  )
+    return <div className="timer-app-title">Timer</div>
 }
