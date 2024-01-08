7924089cdb92e18edfbef262faa6bada4ef22c5b
Adrien Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-01-08 22:24:24 +0100

    Create Chifoumi.jsx
    File for page mini app Chifoumi

---
+++ src/pages/Chifoumi.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import TitleApp from '../components/chifoumi/TitleApp'
+
+export default function chifoumi() {
+    const titleMiniApp = TitleApp
+
+    return <div className="chifoumiPage">{titleMiniApp}</div>
+}
