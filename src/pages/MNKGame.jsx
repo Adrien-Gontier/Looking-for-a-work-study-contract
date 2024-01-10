b26e6ac3b0c294fdfe14cd57be8e23543b151649
Adrien Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-01-10 18:37:19 +0100

    Update MNKGame.jsx
    ClassName for tittle mini app m n k game added

--- src/pages/MNKGame.jsx
+++ src/pages/MNKGame.jsx
@@ -4,5 +4,5 @@ import TitleApp from '../components/mnkgame/TitleApp'
 export default function MNKGame() {
     const titleMiniApp = TitleApp
 
-    return <div>{titleMiniApp}</div>
+    return <div className="cmnkGamePage">{titleMiniApp}</div>
 }
