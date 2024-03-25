e2c4f71561c296cdebdf5cde26ac1c565cc892b4
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2024-03-25 19:02:43 +0100

    Update Chifoumi.jsx
    Code format improved

--- src/pages/Chifoumi.jsx
+++ src/pages/Chifoumi.jsx
@@ -4,6 +4,9 @@ import TitleApp from '../components/chifoumi/TitleApp'
 export default function Chifoumi() {
     const titleMiniApp = TitleApp
 
-    return <div className="chifoumiPage">
-        <div>{titleMiniApp}</div></div>
+    return (
+        <div className="chifoumiPage">
+            <div>{titleMiniApp}</div>
+        </div>
+    )
 }
